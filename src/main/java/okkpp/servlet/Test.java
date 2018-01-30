89a7c684de7b24e3b037a8aa39c2bf2f2852f927
moqianyi <409896482@qq.com>
2018-01-30 17:44:30 +0800

    test
    减空格

--- src/main/java/okkpp/servlet/Test.java
+++ src/main/java/okkpp/servlet/Test.java
@@ -13,7 +13,6 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet("/Test")
 public class Test extends HttpServlet {
-
 	private static final long serialVersionUID = 1L;
        
     /**
