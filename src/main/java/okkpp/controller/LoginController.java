ac689a963a9cf14c3b2bc01ce0ccfeab31412fdc
AI_DK <1019264386@qq.com>
2018-03-09 15:46:17 +0800

    3.9.3
    project

--- src/main/java/okkpp/controller/LoginController.java
+++ src/main/java/okkpp/controller/LoginController.java
@@ -5,55 +5,60 @@ import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.subject.Subject;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
-* @author duck
-* @date 创建时间：2018年3月8日 上午10:52:23
-*/
+ * @author duck
+ * @date 创建时间：2018年3月8日 上午10:52:23
+ */
 @Controller
+@RequestMapping("/system")
 public class LoginController {
 
-	@RequestMapping(value="login",method=RequestMethod.GET)
+	@RequestMapping(value = "login", method = RequestMethod.GET)
 	public String login() {
 		return "/mui/login";
 	}
-	@RequestMapping(value="/login",method=RequestMethod.POST)
-	public String login(String username,String password) {
+
+	@RequestMapping(value = "login", method = RequestMethod.POST)
+	public String login(String username, String password, Model model) {
 		Subject currentUser = SecurityUtils.getSubject();
-		if(!currentUser.isAuthenticated()) {
+		if (!currentUser.isAuthenticated()) {
 			UsernamePasswordToken token = new UsernamePasswordToken(username, password);
 			token.setRememberMe(true);
 			try {
 				currentUser.login(token);
 			} catch (AuthenticationException e) {
-				System.out.println("登录失败");
+				System.out.println("登录失败:" + e.getMessage());
+				if(e.getMessage()==null||e.getMessage()=="")
+					model.addAttribute("msg", "密码错误。");
+				else
+					model.addAttribute("msg", "请确认账号密码。");
 				return "/mui/login";
 			}
-			
+
 		}
-		return "/mui/index";
+		model.addAttribute("msg", "登录成功！");
+		return "/mui/login";
 	}
+
 	@RequestMapping("logout")
 	public String logout() {
 		Subject currentUser = SecurityUtils.getSubject();
 		currentUser.logout();
 		return "/mui/login";
 	}
-	@RequestMapping("/sys_index")
+
+	@RequestMapping("/index")
 	public String index() {
 		return "/mui/index";
 	}
-	@RequestMapping("/sys_mui")
+
+	@RequestMapping("/mui")
 	public String info(String str) {
-		str = "/mui/"+str;
+		str = "/mui/" + str;
 		return str;
-	}
-	@RequestMapping("testjson")
-	@ResponseBody
-	public String testJson() {
-		return "test json";
 	}
 }
--- src/main/webapp/WEB-INF/jsp/mui/info.jsp
+++ src/main/webapp/WEB-INF/jsp/mui/info.jsp
@@ -1,5 +1,4 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"
-	pageEncoding="UTF-8"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <!DOCTYPE html>
 <html lang="zh-cn">
@@ -11,10 +10,10 @@
 <meta name="renderer" content="webkit">
 <title>网站信息</title>
 
-<link rel="stylesheet" href="MUI/css/pintuer.css">
-<link rel="stylesheet" href="MUI/css/admin.css">
-<script src="MUI/js/jquery.js"></script>
-<script src="MUI/js/jquery.js"></script>
+<link rel="stylesheet" href="../MUI/css/pintuer.css">
+<link rel="stylesheet" href="../MUI/css/admin.css">
+<script src="../MUI/js/jquery.js"></script>
+<script src="../MUI/js/jquery.js"></script>
 </head>
 <body>
 	<div class="panel admin-panel">
