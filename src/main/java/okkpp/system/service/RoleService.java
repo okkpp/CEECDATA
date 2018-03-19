a55b258e44bd18517db0eee5e632c04af20b4d76
AI_DK <1019264386@qq.com>
2018-03-19 10:49:16 +0800

    3.19.1

--- src/main/java/okkpp/system/service/RoleService.java
+++ src/main/java/okkpp/system/service/RoleService.java
@@ -1,10 +1,17 @@
 package okkpp.system.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+
 import okkpp.system.dao.RoleMapper;
+import okkpp.system.model.Role;
+import okkpp.utils.ExampleBuilder;
 
 /**
 * @author duck
@@ -16,4 +23,22 @@ public class RoleService {
 
 	@Autowired
 	RoleMapper mapper;
+	
+	public List<Role> listRoles(Role role) {
+		ExampleBuilder example = new ExampleBuilder(role.getClass());
+		example.andEqualTo(role);
+		return mapper.selectByExample(example);
+	}
+
+	public List<Role> getRolesByUserId(int userId) {
+		return mapper.getRolesByUserId(userId);
+	}
+
+	public PageInfo<Role> listRolesPaged(int page, int pageSize, Role role) {
+		ExampleBuilder example = new ExampleBuilder(role.getClass());
+		example.andEqualTo(role);
+		PageHelper.startPage(page, pageSize);
+		List<Role> roles = mapper.selectByExample(example);
+		return new PageInfo<Role>(roles);
+	}
 }
--- src/main/java/okkpp/system/service/UserRoleService.java
+++ src/main/java/okkpp/system/service/UserRoleService.java
@@ -1,10 +1,15 @@
 package okkpp.system.service;
 
+import java.util.HashSet;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import okkpp.system.dao.UserRoleMapper;
+import okkpp.system.model.Role;
+import okkpp.system.model.User;
+import okkpp.system.model.UserRole;
 
 /**
 * @author duck
@@ -17,4 +22,23 @@ public class UserRoleService {
 	@Autowired
 	UserRoleMapper mapper;
 	
+	public void saveUserRoles(User user) {
+		mapper.deleteUserRolesByUserId(user.getId());
+		for(Role role:new HashSet<Role>(user.getRoles())){
+			UserRole userRole = new UserRole();
+			userRole.setUserId(user.getId());
+			userRole.setRoleId(role.getId());
+			mapper.insert(userRole);
+		}
+	}
+
+	public void saveRoleUsers(Role role) {
+		mapper.deleteRoleUsersByRoleId(role.getId());
+		for (User user : new HashSet<User>(role.getUsers())) {
+			UserRole userRole = new UserRole();
+			userRole.setUserId(user.getId());
+			userRole.setRoleId(role.getId());
+			mapper.insert(userRole);
+		}
+	}
 }
