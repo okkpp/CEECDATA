e2320eefdae1b993da01566b6587eca85971472d
AI_DK <1019264386@qq.com>
2018-03-20 15:47:10 +0800

    3.20.6

--- src/main/java/okkpp/controller/PriceController.java
+++ src/main/java/okkpp/controller/PriceController.java
@@ -3,11 +3,9 @@ package okkpp.controller;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.ibatis.annotations.Case;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
