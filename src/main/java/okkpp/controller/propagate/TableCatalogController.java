2551a44d3b17722c189ac308701da06f734d5c10
AI_DK <1019264386@qq.com>
2018-03-30 17:20:50 +0800

    3.30.6

--- src/main/java/okkpp/controller/propagate/TableCatalogController.java
+++ src/main/java/okkpp/controller/propagate/TableCatalogController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -23,9 +24,9 @@ public class TableCatalogController {
 	TableCatalogService service;
 	
 	@RequestMapping("/render/{catalogId}")
-	public String page(@PathVariable(value="catalogId")int catalogId) {
-		
-		return "";
+	public String page(@PathVariable(value="catalogId")int catalogId,Model model) {
+		model.addAttribute("catalog", service.selectByKey(catalogId));
+		return "/propagate/catalog";
 	}
 	@RequestMapping("/list")
 	@ResponseBody
