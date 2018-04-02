91394f110b0a21bb02fc0b7180927afe2b8810bb
moqianyier123 <409896482@qq.com>
2018-04-02 17:11:36 +0800

    zqw-4.2.2

--- src/main/java/okkpp/utils/TestClass.java
+++ src/main/java/okkpp/utils/TestClass.java
@@ -1,12 +1,35 @@
 package okkpp.utils;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import okkpp.dao.ContentMapper;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:applicationContext.xml")
 public class TestClass {
-	public static void main(String[] args) {
-		String str = "GDP";
-		String str2 = "gDP";
-		System.out.println("GDP : " + str.substring(0,1));
-		System.out.println("gDP : " + str2.substring(0,1));
-		System.out.println(str.substring(0,1).equals(str2.substring(0,1)));
-		System.out.println(str.substring(0,1).equals(str2.substring(0,1).toUpperCase()));
+
+	@Autowired
+	ContentMapper mapper;
+
+	@Test
+	public void showTablesWithComment() {
+		List<HashMap<String, String>> columns = null;
+		HashMap<String, String> comments = null;
+		String k = null;
+		columns = mapper.showColumnsWithComment("t_price_consumer");
+		System.out.println(columns);
+		comments = new HashMap<>();
+		for (Map<String, String> column : columns) {
+			k = column.get("Field");
+			if (!(k.equals("country") || k.equals("year") || k.equals("updated") || k.equals("sort")))
+			comments.put(k,column.get("Comment"));
+			
+		}
 	}
 }
